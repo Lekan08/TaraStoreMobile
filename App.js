226c73be0e4582b4eb07a4012563a16263917e8d
adekunbee <darenofisat06@gmail.com>
2022-10-22 16:14:33 +0100

    login page

--- App.js
+++ App.js
@@ -13,188 +13,179 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
 } from "react-native";
-import Header from "./components/header";
-import TodoItem from "./components/todoItem";
-import AddTodo from "./components/addTodo";
-import Sandbox from "./components/sandbox";
+// import Header from "./components/header";
+// import TodoItem from "./components/todoItem";
+// import AddTodo from "./components/addTodo";
+// import Sandbox from "./components/sandbox";
 
 export default function App() {
-  const [username, setUsername] = useState("Daniel");
-  const [Pasword, setPassword] = useState("");
-  const [RPasword, setRPassword] = useState("");
-  const [todos, setTodos] = useState([
-    { text: "bolu is dancing one kind", id: 1 },
-    { text: "emmanuel don cast oooooo", id: 2 },
-    { text: "daniel has landed a big role", id: 3 },
-    { text: "anthony is my father's name", id: 4 },
-  ]);
-  const [countx, setCountx] = useState(todos.length);
+  const [usernamex, setUsername] = useState("");
+  const [passwordx, setPassword] = useState("");
+  const [rPasswordx, setRPassword] = useState("");
 
-  const submitHandler = (text) => {
-    if (text.length > 3) {
-      setCountx(countx + 2);
-      setTodos((todo) => {
-        return [{ text: text, id: countx + 2 }, ...todos];
-      });
-    } else {
-      Alert.alert("OOPS!", "Todos must be over 3 characters long", [
-        { text: "UNDERSTOOD", onPress: () => console.log("Alert Closed") },
-      ]);
-    }
+
+  const handlePress = () => {
+    const raw = JSON.stringify({
+      username: usernamex,
+      password: passwordx,
+    });
+    const myHeaders = {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    };
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+    };
+    fetch("https://mywebsite.com/endpoint/", requestOptions)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => {
+      console.log(err);
+     });
   };
 
-  const pressHandler = (id) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((todo) => todo.id !== id);
-    });
+  const clickHandler = () => {
+    if (
+      usernamex.length === 0 ||
+      usernamex === "" ||
+      passwordx.length === 0 ||
+      passwordx === "" ||
+      rPasswordx.length === 0 ||
+      rPasswordx === ""
+    ) {
+      Alert.alert("Damm", "You can't fuckin leave this place empty dude!!");
+    } else {
+      handlePress();
+    }
   };
 
   return (
     // <Sandbox />
-    <TouchableWithoutFeedback
-      onPress={() => {
-        Keyboard.dismiss();
-        console.log("keyboard dismissed");
-      }}
-    >
-      <View style={styles.container}>
-        {/* header */}
-        <Header />
-        <View style={styles.content}>
-          {/** to form */}
-          <AddTodo submitHandler={submitHandler} />
-          <View style={styles.list}>
-            <FlatList
-              data={todos}
-              keyExtractor={(item) => item.id}
-              renderItem={({ item }) => (
-                <TodoItem item={item} pressHandler={pressHandler} />
-              )}
-            />
-          </View>
+    <View style={styles.container}>
+      <ScrollView>
+        <View>
+          <Image source={require("./images/house_of_tara_logo.png")} />
+        </View>
+        <View style={{ borderRadius: 5 }}>
+          <Text
+            style={{
+              fontSize: 40,
+              fontWeight: "900",
+              color: "#ffffff",
+              paddingHorizontal: 0,
+              paddingTop: 40,
+              fontFamily: "serif",
+              width: 300,
+            }}
+          >
+            Let’s Help You Find Your Match
+          </Text>
+        </View>
+        <Text style={{ color: "#ffffff" }}>
+          Meeting the perfect one shouldn’t be a hassle.
+        </Text>
+        <View style={{ paddingTop: 40 }}>
+          <Text style={styles.inputText}>Username:</Text>
+          <TextInput
+            keyboardType="default"
+            placeholder="Username"
+            value={usernamex}
+            onChangeText={(value) => setUsername(value)}
+            style={styles.input}
+            placeholderTextColor={"#777"}
+          />
+          <Text style={styles.inputText}>Password:</Text>
+          <TextInput
+            placeholder="Password"
+            value={passwordx}
+            onChangeText={(value) => setPassword(value)}
+            style={styles.input}
+            secureTextEntry={true}
+            placeholderTextColor={"#777"}
+          />
+          <Text style={styles.inputText}>Retype Password:</Text>
+          <TextInput
+            placeholder="Retype password"
+            value={rPasswordx}
+            onChangeText={(value) => setRPassword(value)}
+            style={styles.input}
+            secureTextEntry={true}
+            placeholderTextColor={"#777"}
+          />
+          <TouchableOpacity onPress={clickHandler}>
+            <View style={styles.loginButton}>
+              <Text style={styles.loginText}>LOGIN</Text>
+            </View>
+          </TouchableOpacity>
         </View>
 
         {/* <View>
-          {people.map((item) => {
-            return (
-              <View key={item.key}>
-                <Text style={styles.item}>{item.name}</Text>
-              </View>
-            );
-          })}
-        </View> */}
-      </View>
-    </TouchableWithoutFeedback>
+      {people.map((item) => {
+        return (
+          <View key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        );
+      })}
+    </View> */}
+      </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#0F0F0F",
-  },
-  content: {
-    padding: 40,
-  },
-  list: { marginTop: 20 },
   // container: {
   //   flex: 1,
   //   backgroundColor: "#0F0F0F",
-  //   alignItems: "center",
-  //   // justifyContent: "center",
   // },
-  // buttonContainer: {
-  //   marginTop: 20,
-  //   padding: 20,
+  // content: {
+  //   padding: 40,
   // },
-  // input: {
-  //   borderWidth: 1,
-  //   borderColor: "#777",
-  //   padding: 8,
-  //   margin: 5,
-  //   width: 300,
-  //   color: "#777",
-  //   borderRadius: 50,
-  // },
-  // inputText: {
-  //   marginTop: 10,
-  //   alignSelf: "center",
-  //   color: "#fff",
-  // },
-  // item: {
-  //   padding: 30,
-  //   marginTop: 24,
-  //   backgroundColor: "#F96D02",
-  //   fontSize: 24,
-  //   marginHorizontal: 10,
-  // },
+  // list: { marginTop: 20 },
+  container: {
+    flex: 1,
+    backgroundColor: "#0F0F0F",
+    alignItems: "center",
+    paddingTop: 60,
+    // justifyContent: "center",
+  },
+  buttonContainer: {
+    marginTop: 20,
+    padding: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 5,
+    width: 300,
+    color: "#fff",
+    paddingHorizontal: 20,
+    borderRadius: 50,
+  },
+  loginButton: {
+    padding: 15,
+    marginTop: 30,
+    backgroundColor: "#F96D02",
+    marginHorizontal: 10,
+    borderRadius: 50,
+  },
+  loginText: {
+    textAlign: "center",
+    color: "#fff",
+  },
+  inputText: {
+    marginTop: 10,
+    alignSelf: "center",
+    color: "#fff",
+  },
+  item: {
+    padding: 30,
+    marginTop: 24,
+    backgroundColor: "#F96D02",
+    fontSize: 24,
+    marginHorizontal: 10,
+  },
 });
-
-// <View style={styles.container}>
-//   <ScrollView>
-//     <View>
-//       <Image source={require("./images/house_of_tara_logo.png")} />
-//     </View>
-//     <View style={{ borderRadius: 5 }}>
-//       <Text
-//         style={{
-//           fontSize: 40,
-//           fontWeight: "900",
-//           color: "#ffffff",
-//           paddingHorizontal: 0,
-//           fontFamily: "serif",
-//           width: 300,
-//         }}
-//       >
-//         Let’s Help You Find Your Match
-//       </Text>
-//     </View>
-//     <Text style={{ color: "#ffffff" }}>
-//       Meeting the perfect {username} shouldn’t be a hassle.
-//     </Text>
-//     <View>
-//       <Text style={styles.inputText}>Username:</Text>
-//       <TextInput
-//         keyboardType="default"
-//         placeholder="Username"
-//         onChangeText={(value) => setUsername(value)}
-//         style={styles.input}
-//       />
-//       <Text style={styles.inputText}>Password:</Text>
-//       <TextInput
-//         placeholder="Username"
-//         onChangeText={(value) => setPassword(value)}
-//         style={styles.input}
-//       />
-//       <Text style={styles.inputText}>Retype Password:</Text>
-//       <TextInput
-//         placeholder="Username"
-//         onChangeText={(value) => setRPassword(value)}
-//         style={styles.input}
-//       />
-//       <View style={styles.buttonContainer}>
-//         <Button color="#F96D02" title="Login" onPress={clickHandler} />
-//       </View>
-//     </View>
-
-//     {/* <View>
-//       {people.map((item) => {
-//         return (
-//           <View key={item.key}>
-//             <Text style={styles.item}>{item.name}</Text>
-//           </View>
-//         );
-//       })}
-//     </View> */}
-//   </ScrollView>
-//   <FlatList
-//     numColumns={2}
-//     data={people}
-//     keyExtractor={(item) => item.id}
-//     renderItem={({ item }) => (
-//       <TouchableOpacity onPress={() => pressHandler(item.id)}>
-//         <Text style={styles.item}>{item.name}</Text>
-//       </TouchableOpacity>
-//     )}
-//   />
-// </View>
