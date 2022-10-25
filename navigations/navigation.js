a23f72aa7c5238e903340f5326cac63cb9bfd04c
Danthony023 <dangaAnthony023@gmail.com>
2022-10-25 20:35:00 +0100

    tara store mobile 2

---
+++ navigations/navigation.js
@@ -0,0 +1,53 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Registration from "../screens/registration";
+import Login from "../screens/login";
+import ForgotPassword from "../screens/forgotPassword";
+import Home from "../screens/home";
+
+const Stack = createStackNavigator();
+
+function NavigationStack() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerTintColor: "white",
+          headerStyle: { backgroundColor: "#F96D02", height: 60 },
+        }}
+      >
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            title: "Login",
+            //   headerStyle: { backgroundColor: "tomato" },
+          }}
+        />
+        <Stack.Screen
+          name="Registration"
+          component={Registration}
+          options={{
+            title: "Create An Account",
+            //   headerStyle: { backgroundColor: "tomato" },
+          }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: "Dashboard",
+            //   headerStyle: { backgroundColor: "tomato" },
+          }}
+        />
+        <Stack.Screen
+          name="ForgotPassword"
+          component={ForgotPassword}
+          options={{ title: "Forgot Password" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+export default NavigationStack;
