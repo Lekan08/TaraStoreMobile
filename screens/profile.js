b375451e54436003a3a21b6abcd098e9af4bbac2
mohammed adams <klarion51@gmail.com>
2022-11-09 15:36:24 +0100

    SECONDCOMMIT

--- screens/profile.js
+++ screens/profile.js
@@ -16,3 +16,29 @@ export default function Profile({ navigation }) {
     </View>
   );
 }
+
+const ProfileScreen = props => {
+  const user = useSelector(state => state.users.user);
+  const fetchUser = useSelector(state => state.users.fetchUser);
+  const UserId = props.navigation.getParam('id');
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const loadUser = async () => {
+      try {
+        setIsLoading(true);
+        await dispatch(usersActions.fetchUserData(UserId));
+        setIsLoading(false);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+    loadUser();
+  }, [UserId]);
+
+  return (
+    <View style={styles.container}>
+      <Text> Name: {fetchUser.name}</Text>
+    </View>
+  );
+};
